8e58a50501cd75506c158e60c68461d4c8d3c960
Izaias <izaiasramos123456789@gmail.com>
2025-01-03 09:03:47 -0300

    criado layouts padrão para todas as páginas

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        layout padrão -------->>>>>>>>>>>> crio meu layouts
+        <slot></slot>
+        Rodapé do layout 
+        e no app.vue coloco o: <br/>
+        < NuxtLayout>       <br/> 
+            < NuxtPage />   <br/>
+        < /NuxtLayout>      <br/>
+    </div>
+</template>
