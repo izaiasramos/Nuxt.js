84722a9d614925165f5c3076444deba98d6a09c4
Izaias <izaiasramos123456789@gmail.com>
2025-01-05 10:43:33 -0300

    criado a primeira composables, com a função de contar a quantos dias atras foi postado o video,e no arquivo desejado (Detalhes.vue)  logica para usar a composables.

--- components/video/Detalhes.vue
+++ components/video/Detalhes.vue
@@ -1,7 +1,14 @@
+<script setup lang="ts">
+    const { diasPassados, diferencaEmDias } = useFormatacao();
+    const dataVideo = new Date(2024, 11, 1);
+
+   diasPassados(dataVideo);
+
+</script>
 <template>
     <h1>Componentes - Nuxt 3</h1>
     <h3>Autor: Izaias Ramos</h3>
     <h3>Data: 12/05/2022</h3>
-
+    <p>{{ diferencaEmDias }} dias atras</p>
     <p>para chamar esse componante < VideoDetalhes/> o nome da pasta que ele ta com a primeira letra sendo maiuscula: 'Video' e o nome do componente 'Detalhes'.</p>
 </template>
